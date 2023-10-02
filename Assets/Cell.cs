55c41877748c4a1e26889d1d4a6b55efb802d620
acaValkyrie <acavalkyrie@gmail.com>
2023-10-02 18:56:58 +0900

    Cellクラスをコピーするのではなく古いisAliveを保存してそちらを参照するように変更

--- Assets/Cell.cs
+++ Assets/Cell.cs
@@ -5,20 +5,16 @@ namespace DefaultNamespace
 {
   public class Cell{
     private bool _isAlive;
+    private bool _isAlived;
     private GameObject _gameObject;
     private Renderer _renderer;
+    private Color _initialColor;
     public Cell(GameObject obj)
     {
       _gameObject = obj;
       _renderer = _gameObject.GetComponent<Renderer>();
+      _initialColor = _renderer.material.color;
       this.Disable();
-    }
-
-    public Cell(Cell oldCell)
-    {
-      _gameObject = oldCell.GetObject();
-      _renderer = oldCell.GetRenderer();
-      _isAlive = oldCell.IsAlive();
     }
 
     public GameObject GetObject()
@@ -34,16 +30,21 @@ namespace DefaultNamespace
     public void Disable()
     {
       _isAlive = false;
-      _renderer.material.color = new Color(1, 1, 1, 0.001f);
+      _renderer.material.color = new Color(1, 1, 1, 0.0f);
     }
     public void Enable()
     {
       _isAlive = true;
-      _renderer.material.color = new Color(0.44f, 0.64f, 1f, 0.3f);
+      _renderer.material.color = _initialColor;
     }
-    public bool IsAlive()
+    public bool IsAlived()
     {
-      return _isAlive;
+      return _isAlived;
+    }
+
+    public void ShiftIsAlive()
+    {
+      _isAlived = _isAlive;
     }
   }
 }
